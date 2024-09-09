ce1a5b0e65f571166073f298a7da5ff61897b2f2
Vinay-Krishna <148025816+vinay-krishna1987@users.noreply.github.com>
2024-09-09 13:45:19 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+let imgBox = document.getElementById("img-box");
+let qrImg = document.getElementById("qr-img");
+let qrText = document.getElementById("qr-text");
+
+function generateQR(){
+    if(qrText.value.length > 0){
+
+        qrImg.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value ;
+
+        imgBox.classList.add("show-img");
+    } 
+    else{
+        qrText.classList.add('error');
+        setTimeout(()=> {
+            qrText.classList.remove('error');
+        }, 1000)
+    }
+    
+}
